adce286645123d1d925dd9179162c40da0e7f8f7
prapsky <prapto.fmipaitb@gmail.com>
2019-05-14 23:31:21 +0700

    initial commit

---
+++ app/email.py
@@ -0,0 +1,14 @@
+from threading import Thread
+from flask import current_app
+from flask_mail import Message
+from app import mail
+
+def send_async_email(app, message):
+	with app.app_context():
+		mail.send(message)
+
+def send_email(subject, sender, recipients, text_body, html_body):
+	messsage = Message(subject, sender=sender, recipients=recipients)
+	message.body = text_body
+	message.html = html_body
+	Thread(target=send_async_email, args=(current_app.get_current_object(), message)).start()
